e5757d6e63354f5b14989ca86fe1437e19345605
Jasur98Developer <jasurbekabdimuminov98@gmail.com>
2023-02-13 17:25:43 +0500

    Add project files.

---
+++ WorkProj/Program.cs
@@ -0,0 +1,7 @@
+﻿string foydalanuvchi = "Assalomu alaykum,Yangi foydalanuvchi!";
+
+Console.WriteLine(foydalanuvchi);
+Console.Write("Istimos,Ismingizni kiriting: ");
+string ism=Console.ReadLine();
+Console.WriteLine("Sizning ismingiz: " + ism);
+Console.ReadKey();
